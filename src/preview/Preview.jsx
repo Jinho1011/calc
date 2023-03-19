5334088fc6355860a70ff668777f9c159ba40116
Jeon <gino9940@gmail.com>
2023-03-19 23:02:22 +0900

    feat: add preview

---
+++ src/preview/Preview.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Preview.css";
+
+const Preview = () => {
+  return <iframe className="preview" src="https://jsfiddle.net/about" />;
+};
+
+export default Preview;
